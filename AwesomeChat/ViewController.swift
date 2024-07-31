9b4eaa1a558ced1593e61c5255cbc786f5bf44c2
LinhVu <linhvn2303@gmail.com>
2024-07-31 16:38:22 +0700

    Initial Commit

---
+++ AwesomeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeChat
+//
+//  Created by Linh Vu on 31/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
